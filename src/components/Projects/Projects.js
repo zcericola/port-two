91fafb97339b4befdcfc8b2c8aa84d994969cac9
Zac Cericola <zcericola@gmail.com>
2018-04-26 11:07:56 -0400

    added better project pics

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+const artifact = require('../../assets/artifact.png');
+const casa = require('../../assets/casa.png');
 
 class Projects extends Component {
     render(){
@@ -6,11 +8,10 @@ class Projects extends Component {
         
         <h1 className = 'title'>Projects</h1>
         <div className = 'cards'>
-        <div className = 'card'></div>
-        <div className = 'card'></div>
-        <div className = 'card'></div>
+        <div className = 'card'><img src = {artifact} alt = 'artifact fine goods'/></div>
+        <div className = 'card'><img src = {casa} alt = 'casa connection'/></div>
+        <div className = 'card' id = 'more'><span>More Coming Soon!</span></div>
         </div>      
-        
         
         </div>);
     }
