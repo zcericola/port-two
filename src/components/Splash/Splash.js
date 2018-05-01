1ac47eec186df1a225eac0c475d925c704d1e29d
Zac Cericola <zcericola@gmail.com>
2018-05-01 15:28:44 -0500

    added a smooth scroll

--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -3,14 +3,29 @@ import Header from '../Header/Header';
 
 
 class Splash extends Component {
+    constructor(){
+        super();
+
+    this.pullDown = this.pullDown.bind(this);
+    }
+
+    pullDown(){
+        window.scroll({            
+           left: 0,            
+            top: 1022,
+            behavior: 'smooth'
+        
+        });
+
+    }
+
     render(){
         return(<div className = 'container' id = 'splash'>
            <Header />
            <div id = 'hero-container'>
            <h1 id = 'hero'>ZAC CERICOLA</h1>
            <p id = 'sub-hero'>Full Stack Web Developer</p>
-           {/* <i className="fas fa-arrow-down" aria-hidden="true" id = 'down-arrow'></i> */}
-           <i className="far fa-hand-point-down" id = 'down-arrow'></i>
+           <i className="far fa-hand-point-down" id = 'down-arrow' onClick = {() => {this.pullDown()}}></i>
            </div>
         
         </div>);
