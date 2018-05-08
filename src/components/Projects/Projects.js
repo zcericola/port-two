4932fcd46a484c9684359912b6623d38ae4d9e4e
Zac Cericola <zcericola@gmail.com>
2018-05-07 21:15:10 -0500

    worked on adding hoverable projecdt descriptions

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,9 +8,12 @@ class Projects extends Component {
         return(<div id = 'projects' className = 'container'>        
         <h1 className = 'title'>Projects</h1>
         <div className = 'cards'>
-        <div className = 'card'><img src = {artifact} alt = 'Artifact Fine Goods'/></div>
-        <div className = 'card'><img src = {casa} alt = 'Casa Connection'/></div>
-        <div className = 'card'><img src = {book} alt = 'Book Spy'/></div>
+        <div className = 'card'>
+        <img className = 'project-images' src = {artifact} alt = 'Artifact Fine Goods'/>
+        {/* <div className = 'hover-desc'>This is a test.</div> */}
+        </div>
+        <div className = 'card'><img className = 'project-images' src = {casa} alt = 'Casa Connection'/></div>
+        <div className = 'card'><img className = 'project-images' src = {book} alt = 'Book Spy'/></div>
         {/* <div className = 'card' id = 'more'><span>More Coming Soon!</span></div> */}
         </div>              
         </div>);
