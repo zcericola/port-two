03b548ff9d4320755ddf01185a3124a31d3a4489
Zac Cericola <zcericola@gmail.com>
2018-05-09 11:37:29 -0500

    added hover effect box on projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,10 +10,20 @@ class Projects extends Component {
         <div className = 'cards'>
         <div className = 'card'>
         <img className = 'project-images' src = {artifact} alt = 'Artifact Fine Goods'/>
-        {/* <div className = 'hover-desc'>This is a test.</div> */}
+        <div className = 'hover-desc'>
+        <h2>Artifact Fine Goods</h2>
+        <p>A fully functioning fictional e-commerce website built in a 2 week sprint. Allows a customer browse products, add them to the shopping cart, checkout, and edit profile details. Tech used includes React, Redux, PostgreSQL for database management, Express, Node, and Sass.</p>
+        <span className = 'github-link'><a href = 'https://github.com/zcericola/artifact-project' target = '#'>Github <i class="fa fa-code" aria-hidden="true"></i></a></span>        
         </div>
-        <div className = 'card'><img className = 'project-images' src = {casa} alt = 'Casa Connection'/></div>
-        <div className = 'card'><img className = 'project-images' src = {book} alt = 'Book Spy'/></div>
+        </div>
+        <div className = 'card'><img className = 'project-images' src = {casa} alt = 'Casa Connection'/>
+        <div className = 'hover-desc'><h2>Casa Connection</h2><p>A fully functioning fictional e-commerce website built in a 2 week sprint. Allows a customer browse products, add them to the shopping cart, checkout, and edit profile details. Tech used includes React, Redux, PostgreSQL for database management, Express, Node, and Sass.</p>
+        <span className = 'github-link'><a href = 'https://github.com/rental-management/Casa-Connection' target = '#'>Github <i class="fa fa-code" aria-hidden="true"></i></a></span></div>
+        </div>
+        <div className = 'card'><img className = 'project-images' src = {book} alt = 'Book Spy'/>
+        <div className = 'hover-desc'><h2>Book Spy</h2><p>A fully functioning fictional e-commerce website built in a 2 week sprint. Allows a customer browse products, add them to the shopping cart, checkout, and edit profile details. Tech used includes React, Redux, PostgreSQL for database management, Express, Node, and Sass.</p>
+        <span className = 'github-link'><a href = 'https://github.com/zcericola/artifact-project' target = '#'>Github <i class="fa fa-code" aria-hidden="true"></i></a></span></div>
+        </div>
         {/* <div className = 'card' id = 'more'><span>More Coming Soon!</span></div> */}
         </div>              
         </div>);
