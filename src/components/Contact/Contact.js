f59ae12cff8a2d83f98b30abd5221e197eda1802
Zac Cericola <zcericola@gmail.com>
2018-05-09 21:06:31 -0500

    added form validation to contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,71 +1,134 @@
-import React, {Component} from "react";
-import axios from 'axios';
-import Swal from 'sweetalert2';
+import React, { Component } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 class Contact extends Component {
-    constructor(){
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            name:'',
-            email: '',
-            message: ''
-        }
-      
-        this.handleClick = this.handleClick.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      name: "",
+      email: "",
+      message: ""
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleFormValidation = this.handleFormValidation.bind(this);
+  }
+
+  handleClick() {
+    window.scroll({
+      left: 0,
+      top: 0,
+      behavior: "smooth"
+    });
+  }
+
+  handleFormValidation() {
+    let name = document.forms["contact-form"]["usrName"].value;
+    let email = document.forms["contact-form"]["usrEmail"].value;
+    let message = document.forms["contact-form"]["usrMessage"].value;
+    if (name === "" || email === "" || message === "") {
+      Swal({
+        type: "error",
+        text: "All fields are required. Please fill them out and try again.",
+        background: "#fdfdfd",
+        confirmButtonColor: "#216578",
+        allowEnterKey: true
+      });
+      return false;
     }
+    return true;
+  }
 
- 
+  handleSubmit() {
+    const { name, email, message } = this.state;
+    const validated = this.handleFormValidation();
+    console.log(validated);
 
-    handleClick(){
-        window.scroll({
-            left: 0,
-            top: 0,
-            behavior: 'smooth'
-
+    validated
+      ? axios.post("/contact", { name, email, message }).then(response => {
+          document.querySelector("form").reset();
+          Swal({
+            type: "success",
+            title: "Thanks for reaching out!",
+            text: "I'll be in touch soon.",
+            background: "#fdfdfd",
+            confirmButtonColor: "#216578",
+            allowEnterKey: true
+          });
         })
+      : null;
+  }
 
-    }
-
-    handleSubmit(){
-        const {name, email, message} = this.state;
-        axios.post('/contact', {name, email, message}).then( (response) => {
-            document.querySelector('form').reset();
-            Swal({
-                type: 'success',
-                title: 'Thanks for reaching out!',
-                text: 'I\'ll be in touch soon.', 
-                background: '#fdfdfd',
-                confirmButtonColor: '#216578',
-                allowEnterKey: true,
-            
-                           
-              })
-        })
-
-    }
-
-    render(){
-        
-
-    return(<div className = 'container' id = 'contact'>
-    <h1 className = 'title' id = 'contact-title'>Contact</h1>
-    <h3 className = 'sub-title'>If you want to get in touch with me please shoot me an email with the form below.</h3>
-    <form method = 'post'>
-        <fieldset className = 'contact-form'>
-        <label>Name: </label>
-            <input className = 'contact-input' type=  'text' onChange = {(e) => {this.setState({name: e.target.value})}}/> 
-        <label>Email: </label>           
-            <input className = 'contact-input' type = 'email' onChange = {(e) => {this.setState({email: e.target.value})}}/>   
-        <label>Message: </label>    
-            <textarea type = 'text' rows="4" onChange = {(e) => {this.setState({message: e.target.value})}}/>          
-        </fieldset>
-        <span className = 'btn' onClick = {() => {this.handleSubmit()}}>Send</span>
-    </form>   
-    <i className="fa fa-arrow-circle-up" aria-hidden="false" id = 'scroll-top' onClick = {() => {this.handleClick()}}></i> 
-    </div>);
-    }
+  render() {
+    return (
+      <div className="container" id="contact">
+        <h1 className="title" id="contact-title">
+          Contact
+        </h1>
+        <h3 className="sub-title">
+          If you want to get in touch with me please shoot me an email with the
+          form below.
+        </h3>
+        <form method="post" name="contact-form">
+          <fieldset className="contact-form">
+            <label>Name: </label>
+            <input
+              className="contact-input"
+              type="text"
+              onChange={e => {
+                this.setState({ name: e.target.value });
+              }}
+              required
+              autoComplete="on"
+              name="usrName"
+            />
+            <label>Email: </label>
+            <input
+              className="contact-input"
+              type="email"
+              onChange={e => {
+                this.setState({ email: e.target.value });
+              }}
+              required
+              autoComplete="on"
+              name="usrEmail"
+            />
+            <label>Message: </label>
+            <textarea
+              type="text"
+              rows="4"
+              onChange={e => {
+                this.setState({ message: e.target.value });
+              }}
+              required
+              autoComplete="on"
+              name="usrMessage"
+            />
+          </fieldset>
+          <span
+            className="btn"
+            onClick={() => {
+              this.handleSubmit();
+            }}
+            value="submit"
+          >
+            Send
+          </span>
+        </form>
+        <i
+          className="fa fa-arrow-circle-up"
+          aria-hidden="false"
+          id="scroll-top"
+          onClick={() => {
+            this.handleClick();
+          }}
+        />
+      </div>
+    );
+  }
 }
 
 export default Contact;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,64 +1,72 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
 
 class Header extends Component {
-    constructor(){
-        super();
+  constructor() {
+    super();
 
-        this.handleMenuClick = this.handleMenuClick.bind(this);
-        this.handleClose = this.handleClose.bind(this);
+    this.handleMenuClick = this.handleMenuClick.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleMenuClick() {
+    const hamburger = document.getElementById("menu");
+    const actualMenu = document.getElementById("mobile-drop-down");
+    const logo = document.getElementById("logo");
+    if (actualMenu.style.display === "none") {
+      logo.style.display = "none";
+      hamburger.style.display = "none";
+      actualMenu.style.display = "block";
+      actualMenu.style.visibility = "visible";
+    } else {
+      actualMenu.style.display = "none";
+      logo.style.display = "flex";
+      hamburger.style.display = "flex";
     }
+  }
 
-    handleMenuClick(){
-        const hamburger = document.getElementById('menu');
-        const actualMenu = document.getElementById('mobile-drop-down');
-        const logo = document.getElementById('logo');
-        if(actualMenu.style.display === 'none'){
-            logo.style.display = 'none';
-            hamburger.style.display = 'none';
-            actualMenu.style.display = 'block';
-            
-            
-        }
-        else {
-            actualMenu.style.display = 'none'
-            logo.style.display = 'flex';
-            hamburger.style.display = 'flex';
-        }
-        
+  handleClose() {
+    const hamburger = document.getElementById("menu");
+    const actualMenu = document.getElementById("mobile-drop-down");
+    const logo = document.getElementById("logo");
+    actualMenu.style.display = "none";
+    logo.style.display = "flex";
+    hamburger.style.display = "flex";
+  }
 
-    }
-
-    handleClose(){      
-        const hamburger = document.getElementById('menu');
-        const actualMenu = document.getElementById('mobile-drop-down');
-        const logo = document.getElementById('logo');
-        actualMenu.style.display = 'none';
-        logo.style.display = 'flex';
-        hamburger.style.display = 'flex';
-        
-    }
-
-    render(){
-        return(<div className = 'container' id = 'header'>
-        <div id = 'logo'>
-        <span id = 'z-logo'>Z</span>
-        <i className="far fa-moon" id = 'moon'></i>
+  render() {
+    return (
+      <div className="container" id="header">
+        <div id="logo">
+          <span id="z-logo">Z</span>
+          <i className="far fa-moon" id="moon" />
         </div>
-        <i className="fas fa-bars" id = 'menu' aria-hidden= 'false' onClick = {() => this.handleMenuClick()}></i>
-        <div id = 'mobile-drop-down'>
-        <i className="fas fa-times" id = 'close-btn' onClick = {() => this.handleClose()}></i>
-        <ul>
-            <a href = '#about'><li>About</li></a>
-            <a href = '#projects'><li>Projects</li></a>
-            <a href ='#contact'><li>Contact</li></a>
-        </ul>
+        <i
+          className="fas fa-bars"
+          id="menu"
+          aria-hidden="false"
+          onClick={() => this.handleMenuClick()}
+        />
+        <div id="mobile-drop-down">
+          <i
+            className="fas fa-times"
+            id="close-btn"
+            onClick={() => this.handleClose()}
+          />
+          <ul>
+            <a href="#about" onClick={() => this.handleClose()}>
+              <li>About</li>
+            </a>
+            <a href="#projects" onClick={() => this.handleClose()}>
+              <li>Projects</li>
+            </a>
+            <a href="#contact" onClick={() => this.handleClose()}>
+              <li>Contact</li>
+            </a>
+          </ul>
         </div>
-        </div>
-        );
-    }
-
-
+      </div>
+    );
+  }
 }
 
 export default Header;
